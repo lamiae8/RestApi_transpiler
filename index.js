4812a042d256454e24cbd23f677747f1f9b74e8d
lamiae <lamiaemouddene@gmail.com>
2022-08-25 02:27:19 +0100

    transpile text

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {nanoid}= require('nanoid');
 const express = require('express');
 var FormData = require('form-data');
 const path = require('path');
+var fs = require("fs");
 const app =express();
 const cors =require('cors');
 var exec = require('child_process').exec;
@@ -99,3 +100,39 @@ app.post('/post',(req,res)=>{
 
     })
     
+    
+    app.post('/transpileText',(req,res,next) =>{
+
+       const Text =req.body.text;
+        console.log(Text);
+const i= nanoid();
+var writeStream = fs.createWriteStream(`uploads/inputText_${i}.alfa`);
+writeStream.write(Text);
+
+writeStream.end();
+
+
+// convert the file writeStream to solidity 
+exec(`export LD_LIBRARY_PATH=/home/lamiae/Downloads/restApi/lib && LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/libantlr4-runtime.so.4.7.1/ ./AlfaToSol ./uploads/inputText_${i}.alfa ./outputs/OutputOf_inputText_${i}.sol`,
+function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout );
+    console.log('stderr: ' + stderr );
+    if (error !== null) {
+         console.log('exec error: ' + error);
+    } 
+    var options = {
+            root: path.join('./outputs')
+        };
+        var fileGen=`OutputOf_inputText_${i}.sol`;
+        res.sendFile(fileGen, options,function (err) {
+            if (err) {
+                next(err);
+            } else {
+                console.log('Sent:'+ fileGen);
+                next();
+            }
+        });
+});
+       
+        
+    });
